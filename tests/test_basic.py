d3fab93784c0ac40b25dc37dcda879ea001816fd
Youngsung Kim <youngsung.kim.act2@gmail.com>
2020-05-22 09:46:50 -0400

    updated setup.py

---
+++ tests/test_basic.py
@@ -0,0 +1,30 @@
+from microapp import MicroappProject
+
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+data = """
+<mydocument has='an attribute'>
+  <and>
+    <many>elements</many>
+    <many>more elements</many>
+  </and>
+  <plus a='complex'>
+    element as well
+  </plus>
+</mydocument>
+"""
+
+
+def test_basic(capsys):
+
+    prj = MicroappProject()
+    cmd = "uxml2dict \"%s\" -p" % data
+
+    ret = prj.main(cmd)
+
+    assert ret == 0
+    captured = capsys.readouterr()
+    assert captured.out.startswith("OrderedDict")
+    assert captured.err == ""
